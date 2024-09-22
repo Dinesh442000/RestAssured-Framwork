a8d6cd01dc002acfd450ec7af2269928256f475a
Dinesh442000 <dchoudhary442000@gmail.com>
2024-09-22 12:24:56 +0530

    1st commit

---
+++ src/test/java/api/endpoints/UserEndPoints.java
@@ -0,0 +1,53 @@
+package api.endpoints;
+import static io.restassured.RestAssured.given;
+
+import api.payload.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+//created for perfrom crud operation 
+public class UserEndPoints {
+	//create user 
+	public static Response createUser(User payload) {
+		Response response=given()
+			.accept(ContentType.JSON)	
+			.contentType(ContentType.JSON)
+			.body(payload)
+		.when()
+			.post(Routes.post_url);
+		
+		return response;
+	}
+	
+
+		//create user 
+		public static Response readUser(String userName) {
+			Response response=given()
+				.pathParam("username",userName)
+			.when()
+				.get(Routes.get_url);
+			
+			return response;
+		}
+		
+		public static Response updateUser(String userName,User payload) {
+			Response response=given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.pathParam("username",userName)
+				.body(payload)
+			.when()
+				.put(Routes.update_url);
+			
+			return response;
+		}
+		
+		public static Response deleteUser(String userName) {
+			Response response=given()
+				.pathParam("username",userName)
+			.when()
+				.delete(Routes.delete_url);
+			
+			return response;
+		}
+}
